b386d3403db9554482eb083068ab5aed16c0e036
Jesus Tamba <99983151+Jesusdasilva2001@users.noreply.github.com>
2023-09-11 17:43:54 +0100

    Ajustes na class Usuario e fun main

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,25 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    inscritos.add(usuario)
+        println("usuario ${usuario.nome} matriculado na Formacao $nome"}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução á programação", 60)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 90)
+    val conteudo3 = ConteudoEducacional("Algoritmos Avançados", 120)
+
+    // Criando uma formação
+    val Formacao = Formacao("Desenvolvimento de software", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+
+    //Criando alguns usuários
+    val usuario1 = Usuario("Jesus")
+    val usuario2 = Usuario("Óscar")
+
+    // Realizando as matriculas 
+    Formacao.matricular(usuario1)
+    Formacao.matricular(usuario2)   
 }
